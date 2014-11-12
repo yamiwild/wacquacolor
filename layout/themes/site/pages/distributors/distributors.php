3aa54820f89d5510d3afa94d8342624456648299
yamiwild <caroline.particular@gmail.com>
2014-11-12 17:14:56 -0200

    Atualização
    Inseri a formatação da página Distribuidores

--- layout/themes/site/pages/distributors/distributors.php
+++ layout/themes/site/pages/distributors/distributors.php
@@ -1,11 +1,63 @@
-    <div class="after">
+      <div class="after">
     
     <div class="inter">
     <h1>Distribuidores</h1>
-	</div>
+    
+      <div class="info">
+        <img /> <h4>Nome da Empresa</h4>
+        <p>descrição, descrição, descrição, descrição, descrição</p>
+        <a href="#" target="_blank">www.site.com.br</a>
+      </div>
 
+      <div class="info">
+        <img /> <h4>Nome da Empresa</h4>
+        <p>descrição, descrição, descrição, descrição, descrição</p>
+        <a href="#" target="_blank">www.site.com.br</a>
+      </div>
+
+
+      <div class="info">
+        <img /> <h4>Nome da Empresa</h4>
+        <p>descrição, descrição, descrição, descrição, descrição</p>
+        <a href="#" target="_blank">www.site.com.br</a>
+      </div>
+
+
+      <div class="info">
+        <img /> <h4>Nome da Empresa</h4>
+        <p>descrição, descrição, descrição, descrição, descrição</p>
+        <a href="#" target="_blank">www.site.com.br</a>
+      </div>
+
+
+      <div class="info">
+        <img /> <h4>Nome da Empresa</h4>
+        <p>descrição, descrição, descrição, descrição, descrição</p>
+        <a href="#" target="_blank">www.site.com.br</a>
+      </div>
+
+
+      <div class="info">
+        <img /> <h4>Nome da Empresa</h4>
+        <p>descrição, descrição, descrição, descrição, descrição</p>
+        <a href="#" target="_blank">www.site.com.br</a>
+      </div>
+
+
+      <div class="info">
+        <img /> <h4>Nome da Empresa</h4>
+        <p>descrição, descrição, descrição, descrição, descrição</p>
+        <a href="#" target="_blank">www.site.com.br</a>
+      </div>
+
+      <div class="info">
+        <img /> <h4>Nome da Empresa</h4>
+        <p>descrição, descrição, descrição, descrição, descrição</p>
+        <a href="#" target="_blank">www.site.com.br</a>
+      </div>
     
     
+	</div>
     
     </div>
     
